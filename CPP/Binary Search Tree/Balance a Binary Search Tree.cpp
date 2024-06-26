0afacaec2e658cb818d90a65a701f0653fa2758d
Nitish Kumar Gupta <76789784+Nitish252000@users.noreply.github.com>
2024-06-26 10:42:21 +0530

    Create Balance a Binary Search Tree
    Given the root of a binary search tree, return a balanced binary search tree with the same node values. If there is more than one answer, return any of them.
    
    A binary search tree is balanced if the depth of the two subtrees of every node never differs by more than 1.

---
+++ CPP/Binary Search Tree/Balance a Binary Search Tree.cpp
@@ -0,0 +1,20 @@
+void inorder(TreeNode* root, vector<int>&traversal){
+        if(root==NULL)return;
+        inorder(root->left, traversal);
+        traversal.push_back(root->val);
+        inorder(root->right,traversal);
+    }
+    TreeNode* construct(vector<int>&traversal, int start, int end){
+        if(start>end)return NULL;
+        int mid = start+(end-start)/2;
+        TreeNode* node = new TreeNode(traversal[mid]);
+        node->left=construct(traversal,start,mid-1);
+        node->right=construct(traversal,mid+1,end);
+        return node;
+    }
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<int>traversal;
+        inorder(root,traversal);
+        int n = traversal.size();
+        return construct(traversal, 0, n-1);
+    }
