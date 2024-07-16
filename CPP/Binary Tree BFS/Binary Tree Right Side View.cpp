ab30fabdd5e6fcc8989d36641ed39a1b5cf08a5b
Nitish Kumar Gupta <76789784+Nitish252000@users.noreply.github.com>
2024-07-16 15:07:19 +0530

    Update Binary Tree Right Side View.cpp

--- CPP/Binary Tree BFS/Binary Tree Right Side View.cpp
+++ CPP/Binary Tree BFS/Binary Tree Right Side View.cpp
@@ -1,22 +1,24 @@
- vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        if (root == NULL)
-            return ans;
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            int s = q.size();
-            for (int i = 0; i < s; i++) {
-                TreeNode* temp = q.front();
-                q.pop();
-                if (i == s - 1) {
-                    ans.push_back(temp->val); // Add the rightmost node to the result
-                }
-                if (temp->left)
-                    q.push(temp->left);
-                if (temp->right)
-                    q.push(temp->right);
-            }
-        }
+vector<int> rightSideView(TreeNode* root) {
+    vector<int> ans;  // Initialize the result vector
+    if (root == NULL)  // If the root is NULL, return the empty result vector
         return ans;
+    
+    queue<TreeNode*> q;  // Initialize the queue
+    q.push(root);  // Push the root node to the queue
+    
+    while (!q.empty()) {  // Continue until the queue is empty
+        int s = q.size();  // Get the number of nodes at the current level
+        for (int i = 0; i < s; i++) {  // Iterate through all nodes at the current level
+            TreeNode* temp = q.front();  // Get the front node in the queue
+            q.pop();  // Remove the front node from the queue
+            if (i == s - 1) {  // If it's the last node at this level
+                ans.push_back(temp->val);  // Add it to the result vector
+            }
+            if (temp->left)  // If the left child exists
+                q.push(temp->left);  // Push the left child to the queue
+            if (temp->right)  // If the right child exists
+                q.push(temp->right);  // Push the right child to the queue
+        }
+    }
+    return ans;  // Return the result vector
 }
