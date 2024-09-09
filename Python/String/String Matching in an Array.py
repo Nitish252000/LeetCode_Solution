2d25d6c1d398d249c2567fb3dc2c971ebbf62a63
Nitish Kumar Gupta <76789784+Nitish252000@users.noreply.github.com>
2024-09-09 17:12:14 +0530

    Update String Matching in an Array.py

--- Python/String/String Matching in an Array.py
+++ Python/String/String Matching in an Array.py
@@ -14,3 +14,12 @@ class Solution:
                 li.append(words[i])
             i=i+1
         return li
+
+############ Methpd 2-------------------
+ # def stringMatching(self, words: List[str]) -> List[str]:
+ #        ans =set()
+ #        for i in range(len(words)):
+ #            for j in range(len(words)):
+ #                if i!=j and words[i] in words[j]:
+ #                    ans.add(words[i])
+ #        return list(ans)
