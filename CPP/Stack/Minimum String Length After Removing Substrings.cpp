be990c1b271e2688141238aa7d461a4add37b07e
Nitish Kumar Gupta <76789784+Nitish252000@users.noreply.github.com>
2024-10-07 11:00:20 +0530

    Create Minimum String Length After Removing Substrings
    You are given a string s consisting only of uppercase English letters.
    
    You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
    
    Return the minimum possible length of the resulting string that you can obtain.
    
    Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.

---
+++ CPP/Stack/Minimum String Length After Removing Substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (!st.empty() && st.top() == 'A') {
+                if (s[i] == 'B')
+                    st.pop();
+                else
+                    st.push(s[i]);
+            }
+            else if (!st.empty() && st.top() == 'C') {
+                if (s[i] == 'D')
+                    st.pop();
+                else
+                    st.push(s[i]);
+            } else {
+                st.push(s[i]);
+            }
+        }
+        return st.size();
+    }
+};
