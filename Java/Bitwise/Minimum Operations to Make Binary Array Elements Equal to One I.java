0d216f793c80d023bedb06d7e8283ec059903a45
Nitish Kumar Gupta <76789784+Nitish252000@users.noreply.github.com>
2025-03-19 12:32:56 +0530

    Minimum Operations to Make Binary Array Elements Equal to One I
    You are given a binary array nums.
    
    You can do the following operation on the array any number of times (possibly zero):
    
    Choose any 3 consecutive elements from the array and flip all of them.
    Flipping an element means changing its value from 0 to 1, and from 1 to 0.
    
    Return the minimum number of operations required to make all elements in nums equal to 1. If it is impossible, return -1.

---
+++ Java/Bitwise/Minimum Operations to Make Binary Array Elements Equal to One I.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int cnt =0;
+        for(int i=0;i<=nums.length-3;i++){
+            if(nums[i]==0){
+                nums[i]^=1;
+                nums[i+1]^=1;
+                nums[i+2]^=1;
+                cnt++;
+            }
+        }
+        for(int i:nums){
+            if(i==0)return -1;
+        }
+        return cnt;
+    }
+}
