584f110afe826317964bf9cc6079b30a1e3ab765
Nitish Kumar Gupta <76789784+Nitish252000@users.noreply.github.com>
2025-08-21 12:07:52 +0530

    Check if Array Is Sorted and Rotated

--- Python/list/Check if Array Is Sorted and Rotated.py
+++ Python/list/Check if Array Is Sorted and Rotated.py
@@ -1,14 +1,32 @@
-class Solution {
-public:
-    bool check(vector<int>& nums) {
-        int n=nums.size();
-        int cnt=0;
-        for(int i=0;i<n;i++) {
-            if(nums[i]>nums[(i+1)%n]){
-                cnt++;
-            }
-        }
-        if(cnt>1)return false;
-        return true;
-    }
-};
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        n=len(nums)
+        cnt=0
+        for i in range(n):
+            if nums[i]>nums[(i+1)%n]:
+                cnt+=1
+        if cnt>1:
+            return False
+        return True
+
+
+# 🔑 Concept
+
+# An array is a sorted and rotated version of a non-decreasing array if at most one "drop" exists.
+
+# A "drop" = a position where nums[i] > nums[i+1].
+
+# If there are more than one drop, then it’s not sorted rotated.
+
+# Special case: We also need to check the last and first element nums[n-1] > nums[0].
+
+
+# 🔎 Explanation:
+
+# Loop through array nums.
+
+# Compare each element with the next (i+1) % n to wrap around.
+
+# Count how many times the order breaks.
+
+# If more than 1 break → return false, else return true.
