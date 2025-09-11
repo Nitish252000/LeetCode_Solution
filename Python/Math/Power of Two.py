559362e0c8608366280139a29a0cc0d2a6bc0b15
Nitish Kumar Gupta <76789784+Nitish252000@users.noreply.github.com>
2025-09-11 10:36:51 +0530

    Power of Two

---
+++ Python/Math/Power of Two.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n<=0:
+            return False
+        return ((n-1)& n)==0
+        
