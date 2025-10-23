e18373b3a1df69b7240e8ea8bac0e031d30c524a
Nitish Kumar Gupta <76789784+Nitish252000@users.noreply.github.com>
2025-10-23 15:48:49 +0530

    Truncate Sentence

--- CPP/String/stringstream/Truncate Sentence.cpp
+++ CPP/String/stringstream/Truncate Sentence.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     string truncateSentence(string s, int k) {
+        // string ans;
+        // stringstream ss(s);
+        // string word;
+        // while (ss >> word && k != 0) {
+        //     ans += word+" ";
+        //     k--;
+        // }
+        // ans.pop_back();
+        // return ans;
+
+        // Method 2: without any in-built function
         string ans;
-        stringstream ss(s);
-        string word;
-        while (ss >> word && k != 0) {
-            ans += word+" ";
-            k--;
+        int word_cnt=0;
+        for(auto ch:s){
+            if(ch==' '){
+                word_cnt++;
+                if(word_cnt==k){
+                    break;
+                }
+            }
+            ans+=ch;
         }
-        ans.pop_back();
         return ans;
     }
 };
