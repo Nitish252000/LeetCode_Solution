09e071cb12aff4c74918939a8829099c9eb41f70
Nitish Kumar Gupta <76789784+Nitish252000@users.noreply.github.com>
2026-01-22 18:14:09 +0530

    Add repeatedCharacter method to find first duplicate
    Implement method to find the first repeated character in a string.

---
+++ Python/list/First Letter to Appear Twice.py
@@ -0,0 +1,10 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        seen = []
+        for ch in s:
+            if ch in seen:
+                return ch
+            else:
+                seen.append(ch)
+        return ' '
+        
