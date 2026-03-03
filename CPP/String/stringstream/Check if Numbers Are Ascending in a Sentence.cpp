dc9369d8b4df846d81d346d1f69600005476ecfb
Nitish Kumar Gupta <76789784+Nitish252000@users.noreply.github.com>
2026-03-03 17:23:23 +0530

    Add function to check ascending numbers in sentence
    Implement a method to check if numbers in a sentence are in ascending order.

---
+++ CPP/String/stringstream/Check if Numbers Are Ascending in a Sentence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool areNumbersAscending(string s) {
+        stringstream ss(s);
+
+        string word;
+        int prev = -1;
+        while(ss>>word){
+            if(isdigit(word[0])){          //isdigit() only works for single character
+                int num = stoi(word);
+                if(num<=prev)return false;
+                prev = num;
+            }
+        }
+        return true;
+    }
+};
